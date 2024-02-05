138049a80bf6b03b13711c438103ce897d8bebc5
Gianluca Melegari <gianlumele.19@gmail.com>
2024-02-04 23:02:14 -0300

    primera subida...

---
+++ Proyecto.NET/BandaMusical.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.NET
+{
+    internal class BandaMusical
+    {
+        public DateTime Fecha { get; set; }
+        public string Titulo { get; set;}
+
+        public int CantCanciones { get; set; }
+         
+    }
+}
---
+++ Proyecto.NET/BandaMusicalSERVICE.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Proyecto.NET
+{
+    internal class BandaMusicalSERVICE
+    {
+        //en esta clase se declaran los metodos para acceder a la base de datos
+
+        public List<BandaMusical> listar()
+        {
+            List<BandaMusical>lista=new List<BandaMusical>();
+
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=DISCOS_DB; integrated security=true";
+                comando.CommandType=System.Data.CommandType.Text;
+                comando.CommandText = "select Titulo, FechaLanzamiento, CantidadCanciones from DISCOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    BandaMusical aux = new BandaMusical();
+
+                    aux.Titulo = (string)lector["Titulo"];
+                    aux.Fecha = (DateTime)lector["FechaLanzamiento"];
+                    aux.CantCanciones = (int)lector["CantidadCanciones"];
+
+                    lista.Add(aux);  
+
+                }
+                conexion.Close();
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+    }
+}
